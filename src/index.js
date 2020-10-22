6513649e2ba3ed2f2f1f55702de6260da16b53ff
Mori 30R <ghahreman00@gmail.com>
2020-10-22 21:49:02 +0330

    Theme added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyle } from "./js/GlobalStyles/Base";
 import { WeatherApp } from './js/components/Weather';
+import * as Theme from "./js/GlobalStyles/Theme";
+import {ThemeProvider} from "styled-components";
+
+const App = () => {
+    return (
+        <>
+            <ThemeProvider theme={Theme}>
+                <GlobalStyle/>
+                <WeatherApp/>
+            </ThemeProvider>
+        </>
+    )
+}
 
 
 ReactDOM.render(
-    <WeatherApp/>,
+    <App/>,
   document.getElementById('root')
 );
---
+++ src/js/GlobalStyles/Theme.js
@@ -0,0 +1,3 @@
+export const DarkBack = '#040404';
+export const DarkText = '#575757';
+export const LightText = '#eee';
