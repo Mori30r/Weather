1d80d0689367543eb344881dff3364e7b969bdfa
Mori 30R <ghahreman00@gmail.com>
2020-10-25 13:24:17 +0330

    hide api keys

--- src/js/Services/GetWeatherData.js
+++ src/js/Services/GetWeatherData.js
@@ -7,12 +7,14 @@ let getWeatherUrl;
 const getIp = () => {
     return publicIp.v4();
 }
+const locationKey = config.LOCATION_KEY
+const weatherKey = config.WEATHER_KEY
 
 export async function getWeather () {
     const ip = await getIp();
-    getCityUrl = `http://api.ipstack.com/${ip}?access_key=94d09010ee5d3975487b51e26f3a1ee4`;
+    getCityUrl = `http://api.ipstack.com/${ip}?access_key=${ location }`;
     const location = await axios.get(getCityUrl);
-    getWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.data["latitude"]}&lon=${location.data["longitude"]}&appid=1b6866d9113d620d6c30d25e3b474830`
+    getWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.data["latitude"]}&lon=${location.data["longitude"]}&appid=${ weatherKey }`
     const weather = await axios.get(getWeatherUrl);
     return weather.data['daily'];
 }
